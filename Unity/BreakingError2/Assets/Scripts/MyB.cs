9939fab121f3fa17c8d0c6bdde5da60a572648ca
nlee013 <valuenacreator@gmail.com>
2021-05-05 20:05:29 +0900

    Convert from Main scene to Load scene

---
+++ Unity/BreakingError2/Assets/Scripts/MyB.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MyB : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public MainUI.BTNType currentType;
+    public Transform ButtonScale;
+    Vector3 defaultScale;
+
+    private void Start()
+    {
+        defaultScale = ButtonScale.localScale;
+    }
+
+    public void OnButtonClick()
+    {
+        switch (currentType)
+        {
+            case MainUI.BTNType.PLAY:
+                SceneLoader.LoadSceneHandle("Play", 0);
+                break;
+
+            case MainUI.BTNType.QUIT:
+                Application.Quit();
+                Debug.Log("finish");
+                break;
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ButtonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonScale.localScale = defaultScale;
+
+    }
+
+}
+
---
+++ Unity/BreakingError2/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Slider progressbar;
+    public Text loadText;
+    public static string loadScene;
+    public static int loadType;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public static void LoadSceneHandle(string _name, int _loadType)
+    {
+        loadScene = _name;
+        loadType = _loadType;
+        SceneManager.LoadScene("Load");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Play");
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            yield return null;
+
+            if (loadType == 0)
+                Debug.Log("new game");
+     
+
+            if (progressbar.value < 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
+            }
+            else if(operation.progress>=0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+            }
+
+            if (progressbar.value >= 1f)
+            {
+                loadText.text = "Press Next";
+            }
+            if (Input.GetKeyDown(KeyCode.Space)&&progressbar.value>=1f&&operation.progress>=.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
+
+
+    }
+}
+
